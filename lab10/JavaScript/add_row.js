b7be5a4c51e83a9d0e017020a24430b67c17df34
Valentine Danilov <valik.danilov9977@gmail.com>
2018-04-23 22:42:12 +0300

    added Lab10-1 finished

--- lab10/JavaScript/add_row.js
+++ lab10/JavaScript/add_row.js
@@ -1,33 +1,40 @@
-var d = document;
+
 function addRow()
 {
-	
+	var d = document;
+	var ava = d.getElementById('avatar').value;
 	var fname = d.getElementById('fname').value;
 	var lname = d.getElementById('lname').value;
 	var group = d.getElementById('group').value;
 	var course = d.getElementById('course').value;
-	var isOnVoenka = d.getElementById('ison').value;
-	alert(fname);
+	var isOnVoenka = d.getElementById('ison').checked;
 	var tbody = d.getElementById('st_table').getElementsByTagName('TBODY')[0];
-
+	
 	var row = d.createElement("TR");
-	body.appendChild(row);
+	tbody.appendChild(row);
 
 	var td1 = d.createElement("TD");
 	var td2 = d.createElement("TD");
 	var td3 = d.createElement("TD");
 	var td4 = d.createElement("TD");
 	var td5 = d.createElement("TD");
+	var td6 = d.createElement("TD");	
 
 	row.appendChild(td1);
 	row.appendChild(td2);
 	row.appendChild(td3);
 	row.appendChild(td4);
 	row.appendChild(td5);
-
+	row.appendChild(td6);
+	
+	
 	td1.innerHTML = fname;
 	td2.innerHTML = lname;
 	td3.innerHTML = group;
 	td4.innerHTML = course;
 	td5.innerHTML = isOnVoenka;
+	if(isOnVoenka==true) td5.innerHTML = 'Yes';
+	if(isOnVoenka==false) td5.innerHTML = 'No';
+	td6.innerHTML = ava;
+	
 }
